7475d557b5713c8ec6db11c46adbbc13ab6cd8d7
Chris Kim <oats87g@gmail.com>
2021-05-20 11:34:41 -0400

    prober fix
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- pkg/prober/prober.go
+++ pkg/prober/prober.go
@@ -70,9 +70,13 @@ func DoProbe(probe Probe, probeStatus *ProbeStatus, initial bool) error {
 		failureThreshold = probe.FailureThreshold
 	}
 
+	logrus.Debugf("probe status: %v")
+	logrus.Debugf("probe status success count: %d", probeStatus.SuccessCount)
+
 	switch probeResult {
 	case k8sprobe.Success:
 		if probeStatus.SuccessCount < probe.SuccessThreshold {
+			logrus.Debugf("probe was successful")
 			probeStatus.SuccessCount = probeStatus.SuccessCount + 1
 			if probeStatus.SuccessCount >= successThreshold {
 				probeStatus.Healthy = true
@@ -80,6 +84,7 @@ func DoProbe(probe Probe, probeStatus *ProbeStatus, initial bool) error {
 		}
 		probeStatus.FailureCount = 0
 	default:
+		logrus.Debugf("probe status failed")
 		if probeStatus.FailureCount < probe.FailureThreshold {
 			probeStatus.FailureCount = probeStatus.FailureCount + 1
 			if probeStatus.FailureCount >= failureThreshold {
