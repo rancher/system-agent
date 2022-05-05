6ce4f9d0154ffbe21503e90411260f752e34d6db
Ross Kirkpatrick <ross.kirkpatrick@suse.com>
2022-05-05 14:11:30 -0400

    ensure we loop over every certificate, add nil check

--- pkg/prober/prober_windows.go
+++ pkg/prober/prober_windows.go
@@ -22,7 +22,7 @@ const (
 // ref: https://docs.microsoft.com/en-us/windows/win32/api/wincrypt/nf-wincrypt-certgetissuercertificatefromstore
 // TODO: Test and remove after system-agent is bumped to go1.18+
 func GetSystemCertPool(probeName string) (*x509.CertPool, error) {
-	logrus.Tracef("[GetSystemCertPoolWindows] building system cert pool for probe (%s)", probeName)
+	logrus.Tracef("[GetSystemCertPoolWindows] building system certContext pool for probe (%s)", probeName)
 	root, err := syscall.UTF16PtrFromString("Root")
 	if err != nil {
 		return nil, fmt.Errorf("[GetSystemCertPoolWindows] unable to return UTF16 pointer: %v", syscall.GetLastError())
@@ -31,45 +31,78 @@ func GetSystemCertPool(probeName string) (*x509.CertPool, error) {
 		return nil, fmt.Errorf("[GetSystemCertPoolWindows] UTF16 pointer for Root returned nil: %v", syscall.GetLastError())
 
 	}
+	// win32 reference: https://docs.microsoft.com/en-us/windows/win32/api/wincrypt/nf-wincrypt-certopensystemstorea
+	// If the function succeeds, it returns a handle to the specified certificate store.
 	storeHandle, err := syscall.CertOpenSystemStore(0, root)
 	if err != nil {
-		return nil, fmt.Errorf("[GetSystemCertPoolWindows] unable to open system cert store: %v", syscall.GetLastError())
+		return nil, fmt.Errorf("[GetSystemCertPoolWindows] unable to open system certContext store: %v", syscall.GetLastError())
 	}
 
 	var certs []*x509.Certificate
-	var cert *syscall.CertContext
+	var certContext *syscall.CertContext
 
-	cert, err = syscall.CertEnumCertificatesInStore(storeHandle, cert)
-	if err != nil {
-		if errno, ok := err.(syscall.Errno); ok {
-			if errno == CRYPT_E_NOT_FOUND {
-				return nil, fmt.Errorf("[GetSystemCertPoolWindows] no certificate context was found for probe (%s)", probeName)
+	// ref for why flags value is 0: https://docs.microsoft.com/en-us/windows/win32/api/wincrypt/nf-wincrypt-certclosestore
+	defer func(store syscall.Handle, flags uint32) {
+		_ = syscall.CertCloseStore(store, flags)
+	}(certContext.Store, 0)
+
+	for {
+		certContext, err = syscall.CertEnumCertificatesInStore(storeHandle, certContext)
+		if err != nil {
+			if errno, ok := err.(syscall.Errno); ok {
+				if errno == CRYPT_E_NOT_FOUND {
+					// the for loop + the syscall for `CertEnumCertificatesInStore` will iterate through
+					// all available certContexts in the specified certificate store and
+					// returns a single certContext containing all certificates in the store
+					// if the error returned here is CRYPTO_E_NOT_FOUND, that indicates no certificates were found.
+					// This happens if the store is empty or if the function reached the end of the store's list.
+					// ref: https://docs.microsoft.com/en-us/windows/win32/api/wincrypt/nf-wincrypt-certenumcertificatesinstore#return-value
+					logrus.Debugf("[GetSystemCertPoolWindows] no certificates were returned from the root CA store for probe (%s), expected a non-empty return value", probeName)
+					break
+				}
 			}
+			logrus.Errorf("[GetSystemCertPoolWindows] unable to enumerate certs in system certContext store for probe (%s): %v", probeName, syscall.GetLastError())
 		}
-		return nil, fmt.Errorf("[GetSystemCertPoolWindows] unable to enumerate certs in system cert store for probe (%s): %v", probeName, syscall.GetLastError())
+		if certContext == nil {
+			logrus.Errorf("[GetSystemCertPoolWindows] certificate context returned from syscall is nil for probe (%s)", probeName)
+			break
+		}
+
+		// buf is a ~1048 kilobyte array that serves as a buffer holding the encoded value
+		// for each CA certificate in the Windows root CA store
+		// we use a binary shift to create a ~1048 Kb buffer (slightly larger than 1 megabyte)
+		// [1 << 20]byte -> (1*2)^20 = 1048576 bytes
+		// maximum size of a Windows certificate store is 16 kilobytes and is not related to number of certificates
+		// (1048576*8)/4096 = 2048 (amount of 4096-bit certificates that can be stored)
+		buf := (*[1 << 20]byte)(unsafe.Pointer(certContext.EncodedCert))[:]
+
+		// buf2 is an array of bytes equal to the length of the certContext pointer
+		// which contains a certificate from the Root CA store
+		buf2 := make([]byte, certContext.Length)
+		copy(buf2, buf)
+
+		// validate the root CA certificate and return a x509.Certificate pointer
+		// that is appended into our array of x509 certificates
+		c, err := x509.ParseCertificate(buf2)
+		if err != nil {
+			return nil, fmt.Errorf("[GetSystemCertPoolWindows] unable to parse x509 certificate for probe (%s): %v", probeName, err)
+		}
+		certs = append(certs, c)
+		logrus.Debugf("[GetSystemCertPoolWindows] Successfully loaded %d certificates from system certContext store for probe (%s)", len(certs), probeName)
 	}
-	if cert == nil {
-		return nil, fmt.Errorf("[GetSystemCertPoolWindows] certificate context returned from syscall is nil for probe (%s)", probeName)
-	}
-	// Copy the buf, since ParseCertificate does not create its own copy.
-	buf := (*[1 << 20]byte)(unsafe.Pointer(cert.EncodedCert))[:]
-	buf2 := make([]byte, cert.Length)
-	copy(buf2, buf)
-	c, err := x509.ParseCertificate(buf2)
-	if err != nil {
-		return nil, fmt.Errorf("[GetSystemCertPoolWindows] unable to parse x509 certificate for probe (%s): %v", probeName, err)
-	}
-	certs = append(certs, c)
-	logrus.Debugf("[GetSystemCertPoolWindows] Successfully loaded %d certificates from system cert store for probe (%s)", len(certs), probeName)
 
 	caCertPool := x509.NewCertPool()
+	if caCertPool == nil {
+		return nil, fmt.Errorf("[GetSystemCertPoolWindows] x509 returned a nil certpool for probe (%s)", probeName)
+	}
+
 	for _, certificate := range certs {
 		if !caCertPool.AppendCertsFromPEM(certificate.RawTBSCertificate) {
-			return nil, fmt.Errorf("[GetSystemCertPoolWindows] unable to append cert with CN [%s] to system cert pool for probe (%s)", c.Subject.CommonName, probeName)
+			return nil, fmt.Errorf("[GetSystemCertPoolWindows] unable to append certContext with CN [%s] to system certContext pool for probe (%s)", certificate.Subject.CommonName, probeName)
 		}
-		logrus.Tracef("[GetSystemCertPoolWindows] successfully appended cert with CN [%s] to system cert pool for probe (%s)", c.Subject.CommonName, probeName)
+		logrus.Tracef("[GetSystemCertPoolWindows] successfully appended certContext with CN [%s] to system certContext pool for probe (%s)", certificate.Subject.CommonName, probeName)
 	}
-	logrus.Infof("[GetSystemCertPoolWindows] Successfully loaded %d certificates into system cert pool for probe (%s)", len(certs), probeName)
+	logrus.Infof("[GetSystemCertPoolWindows] Successfully loaded %d certificates into system certContext pool for probe (%s)", len(certs), probeName)
 
 	return caCertPool, nil
 }
