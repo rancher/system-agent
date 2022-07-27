93a637501c9ca654862c1e5d6611928961c24ca5
Jake Hyde <jakefhyde@gmail.com>
2022-07-27 14:05:03 -0400

    Reuse version if found for binary, and add uninstall to suc entrypoint

--- package/suc/run.sh
+++ package/suc/run.sh
@@ -14,7 +14,9 @@ cleanup() {
 
 cp /opt/rancher-system-agent-suc/install.sh "/host${TMPDIR}"
 cp /opt/rancher-system-agent-suc/rancher-system-agent "/host${TMPDIR}"
+cp /opt/rancher-system-agent-suc/system-agent-uninstall.sh "/host${TMPDIR}/rancher-system-agent-uninstall.sh"
 chmod +x "/host${TMPDIR}/install.sh"
+chmod +x "/host${TMPDIR}/rancher-system-agent-uninstall.sh"
 
 if [ -n "$SYSTEM_UPGRADE_NODE_NAME" ]; then
     NODE_FILE=/host${TMPDIR}/node.yaml
@@ -34,7 +36,9 @@ if [ -n "$SYSTEM_UPGRADE_NODE_NAME" ]; then
 fi
 
 export CATTLE_AGENT_BINARY_LOCAL=true
+export CATTLE_AGENT_UNINSTALL_LOCAL=true
 export CATTLE_AGENT_BINARY_LOCAL_LOCATION=${TMPDIR}/rancher-system-agent
+export CATTLE_AGENT_UNINSTALL_LOCAL_LOCATION=${TMPDIR}/rancher-system-agent-uninstall.sh
 if [ -s /host/etc/systemd/system/rancher-system-agent.env ]; then
   for line in $(grep -v '^#' /host/etc/systemd/system/rancher-system-agent.env); do
     var=${line%%=*}
